301ccdfb4e9c46e934a51f48e87df6283b3b38c5
Aravinda VK <mail@aravindavk.in>
2026-06-12 20:37:38 +0530

    Fix the Translate example (#16)
    ulong was passed to translate function and negative value ignored
    while converting to double.
    
    This was causing unexpected behaviour when the translated values are
    reveresed. For example, `i` was declared as `ulong`
    
    ```
    translate(0, i * 50);
    // draw something(...);
    translate(0, -i * 50);
    ```
    
    Signed-off-by: Aravinda VK <vkaravinda7@gmail.com>

--- examples/translate.d
+++ examples/translate.d
@@ -3,6 +3,8 @@
  +/
 
 import std.stdio;
+import std.format;
+
 import chitra;
 
 void main()
@@ -12,24 +14,28 @@ void main()
     with (ctx)
     {
         // Translate
-        void bar(double percentage)
+        void bar(string label, double percentage)
         {
-            writeln(percentage, width);
+            noStroke;
+            textColor(0);
+            textFont("Inter", 10);
+            text(label, 0, 0);
+
             if (percentage > 50)
                 fill(0, 255, 0);
             else
                 fill(255, 0, 0);
 
-            rect(0, 0, width*percentage/100, 20);
+            rect(0, 20, width * percentage / 100, 20);
         }
 
         double[] values = [93, 70, 41, 56, 77, 21, 87];
         foreach(i, p; values)
         {
-            writeln(i*50);
-            translate(0, i*50);
-            bar(p);
-            translate(0, -i*50);
+            double translateY = i * 50;
+            translate(0, translateY);
+            bar(format("Subject %d", i), p);
+            translate(0, -translateY);
         }
 
         saveAs("output/translate_bar.png");

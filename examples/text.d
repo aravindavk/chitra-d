aeb8f6e483c7bf98286f064da5643863d66de4da
Aravinda VK <vkaravinda7@gmail.com>
2026-07-05 20:16:51 +0530

    Added support for textSize to get size.
    This helps to get the size of text before drawing.
    
    Example usage: Align text to center of the page.
    
    ```d
    auto ctx = new Chitra;
    auto box = ctx.textSize("Hello World!");
    auto x = (ctx.width - box.width) / 2.0;
    auto y = (ctx.height - box.height) / 2.0;
    ctx.text("Hello World!", x, y);
    ```
    
    Signed-off-by: Aravinda VK <vkaravinda7@gmail.com>

--- examples/text.d
+++ examples/text.d
@@ -13,11 +13,19 @@ void main()
     {
         noStroke;
 
-        fill(0, 0, 255, 0.5);
-        rect(10, 76, 380, 5);
 
         textFont("American Typewriter", 50);
         text("Hello World", 10, 10);
+        auto box = textSize("Hello World");
+
+        // Draw text underline same as text width
+        fill(0, 0, 255, 0.5);
+        rect(10, 10+box.height, box.width, 5);
+
+        // Algin Text to the middle of the canvas
+        auto x = (width - box.width) / 2.0;
+        auto y = (height - box.height) / 2.0;
+        text("Hello World", x, y);
 
         saveAs("output/text.png");
     }

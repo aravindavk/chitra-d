5336ec11da18e2b194d73edbe67c2ee092b0b467
Aravinda VK <vkaravinda7@gmail.com>
2026-07-06 23:24:19 +0530

    Text box feature
    Fit the text to the bounding box given as `x, y, w, h`.
    
    ```d
    text("Hello World!...", 10, 10, 600, 300);
    ```
    
    Signed-off-by: Aravinda VK <vkaravinda7@gmail.com>

--- source/chitra/elements/text.d
+++ source/chitra/elements/text.d
@@ -2,12 +2,14 @@ module chitra.elements.text;
 
 import std.format;
 import std.string;
+import std.array;
 
 import chitra.context;
 import chitra.properties;
 import chitra.elements.core;
 import chitra.elements.formatted_strings;
 import chitra.rgba;
+import chitra.elements.markup_tokens;
 
 struct Text
 {
@@ -26,17 +28,94 @@ struct Text
         this.h_ = h;
     }
 
+    Token readyToDraw(Context chitraCtx, string fullMarkup, double w, double h)
+    {
+        if (w == 0 && h == 0)
+            return Token(fullMarkup, "");
+
+        // Check if the height of the rendered text is less than the given height
+        // If height not fits, then convert the Markup text to plain text, so that
+        // word by word we can check the textSize and then render.
+        auto box = size(chitraCtx, chitraCtx.defaultCairoCtx, fullMarkup, w, h);
+        if (box.height <= h)
+            return Token(fullMarkup, "");
+
+        Token outToken;
+        auto tokens = MarkupToken(fullMarkup);
+        while (true)
+        {
+            auto token = tokens.get;
+            auto box1 = size(chitraCtx, chitraCtx.defaultCairoCtx, token.part, w, h);
+            // If the text height is more than the given height, check
+            // if the text till prev word covers the bounding box.
+            if (box1.height > h)
+            {
+                auto prevToken = tokens.getPrev;
+                auto box2 = size(chitraCtx, chitraCtx.defaultCairoCtx, prevToken.part, w, h);
+                // Till Prev word the text is covered, So this
+                // the max we can render
+                if (box2.height <= h)
+                {
+                    outToken = prevToken;
+                    break;
+                }
+
+                // Both the text till current and Prev word is high than
+                // the bounding box. Mark it as High, so that Tokens stream
+                // can continue search (Binary search).
+                tokens.notOkHigh;
+            }
+            else
+            {
+                auto nextToken = tokens.getNext;
+                auto box2 = size(chitraCtx, chitraCtx.defaultCairoCtx, nextToken.part, w, h);
+                // Current word is covered and the next word we
+                // can't cover, so this is the max we can render.
+                if (box2.height >= h)
+                {
+                    outToken = token;
+                    break;
+                }
+
+                // Both the text till current and Next word is low than
+                // the bounding box. Mark it as Low, so that Tokens stream
+                // can continue search (Binary search).
+                tokens.notOkLow;
+            }
+        }
+
+        return outToken;
+    }
+
     void draw(Context chitraCtx, cairo_t* cairoCtx)
     {
         auto x = chitraCtx.correctedSize(x_);
         auto y = chitraCtx.correctedSize(y_);
+        auto w = chitraCtx.correctedSize(w_);
+        auto h = chitraCtx.correctedSize(h_);
+
         cairo_save(cairoCtx);
         // Do not use fill color since the text color will be added
         shapeProps.fill = RGBA(0, 0, 0, 0);
         cairo_set_antialias(cairoCtx, CAIRO_ANTIALIAS_BEST);
         cairo_move_to(cairoCtx, x, y);
         auto layout = pango_cairo_create_layout(cairoCtx);
-        pango_layout_set_markup(layout, txt.content(chitraCtx).toStringz, -1);
+
+        if (w > 0)
+            pango_layout_set_width(layout, cast(int)w * PANGO_SCALE);
+
+        if (h > 0)
+            pango_layout_set_height(layout, cast(int)h * PANGO_SCALE);
+
+        auto fullMarkup = txt.content(chitraCtx);
+
+        // TODO: Add hyphenation function
+        // pango_layout_set_wrap(layout, PANGO_WRAP_CHAR);
+
+        auto token = readyToDraw(chitraCtx, fullMarkup, w, h);
+        pango_layout_set_markup(layout, token.part.toStringz, -1);
+        chitraCtx.overflowMarkup_ = token.rest;
+
         pango_cairo_update_layout(cairoCtx, layout);
         pango_cairo_show_layout(cairoCtx, layout);
         pango_cairo_layout_path(cairoCtx, layout);
@@ -52,18 +131,23 @@ struct Text
 
     Box size(Context chitraCtx, cairo_t* cairoCtx)
     {
+        return size(chitraCtx, cairoCtx, txt.content(chitraCtx), w_, h_);
+    }
+
+    Box size(Context chitraCtx, cairo_t* cairoCtx, string value, double w = 0.0, double h = 0.0)
+    {
         cairo_save(cairoCtx);
         // Do not use fill color since the text color will be added
         cairo_set_antialias(cairoCtx, CAIRO_ANTIALIAS_BEST);
         cairo_move_to(cairoCtx, x_, y_);
         auto layout = pango_cairo_create_layout(cairoCtx);
-        if (w_ > 0)
-            pango_layout_set_width(layout, cast(int)(w_ * PANGO_SCALE));
+        if (w > 0)
+            pango_layout_set_width(layout, cast(int)(w * PANGO_SCALE));
 
-        if (h_ > 0)
-            pango_layout_set_height(layout, cast(int)(h_ * PANGO_SCALE));
+        if (h > 0)
+            pango_layout_set_height(layout, cast(int)(h * PANGO_SCALE));
 
-        pango_layout_set_markup(layout, txt.content(chitraCtx).toStringz, -1);
+        pango_layout_set_markup(layout, value.toStringz, -1);
         pango_cairo_update_layout(cairoCtx, layout);
         pango_cairo_show_layout(cairoCtx, layout);
         pango_cairo_layout_path(cairoCtx, layout);
@@ -73,13 +157,13 @@ struct Text
         else
             cairo_new_path(cairoCtx);
 
-        int w, h;
-        pango_layout_get_size(layout, &w, &h);
+        int w1, h1;
+        pango_layout_get_size(layout, &w1, &h1);
 
         cairo_restore(cairoCtx);
         g_object_unref(layout);
 
-        return Box(x_, y_, (cast(double)w) / PANGO_SCALE, (cast(double)h) / PANGO_SCALE);
+        return Box(x_, y_, (cast(double)w1) / PANGO_SCALE, (cast(double)h1) / PANGO_SCALE);
     }
 }
 
@@ -135,5 +219,10 @@ mixin template textFunctions()
         s.textProps = props;
         s.shapeProps = this.shapeProps;
         return s.size(this, this.defaultCairoCtx);
+    }
+
+    string overflowText()
+    {
+        return this.overflowMarkup_;
     }
 }

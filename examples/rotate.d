fd79b8628c6e3a5feac9b336033ae833d646c24c
Aravinda VK <vkaravinda7@gmail.com>
2026-07-23 11:34:25 +0530

    Add support for `angleMode` for `rotate` command.
    ```
    rotate(PI);
    // Above is same as
    angleMode(DEGREES);
    rotate(180);
    ```
    
    Signed-off-by: Aravinda VK <vkaravinda7@gmail.com>

---
+++ examples/rotate.d
@@ -0,0 +1,40 @@
+/+ dub.sdl:
+ dependency "chitra" path="../"
+ +/
+
+import std.stdio;
+import chitra;
+
+void main()
+{
+    auto ctx = new Chitra();
+
+    with (ctx)
+    {
+        background(255);
+
+        stroke(100);
+
+        auto part = TWO_PI / 12.0;
+        // OR Use:
+        // angleMode(DEGREES);
+        // auto part = 30; // 360 / 12
+
+        foreach(i; 0 .. 12)
+        {
+            saveState;
+            rotate(part * i, width / 2, height / 2);
+            line(width / 2, height / 2, width, height / 2);
+            restoreState;
+        }
+
+        // OR without saveState
+        // foreach(_i; 0 .. 12)
+        // {
+        //     rotate(part, width / 2, height / 2);
+        //     line(width / 2, height / 2, width, height / 2);
+        // }
+
+        saveAs("output/rotate.png", resolution: 72);
+    }
+}
